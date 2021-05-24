8c50521c9ff00d0f4e746a82b2b6f2b54a45d474
SH0909 <poa0909@gmail.com>
2021-05-24 17:36:47 +0900

    test

--- blob-quickstart-v12/blob.py
+++ blob-quickstart-v12/blob.py
@@ -3,35 +3,29 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 
 try:
     print("Azure Blob Storage v" + __version__ + " - Python quickstart sample")
-
-    # Quick start code goes here
-    # Retrieve the connection string for use with the application. The storage
-    # connection string is stored in an environment variable on the machine
-    # running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
-    # created after the application is launched in a console or with Visual Studio,
-    # the shell or application needs to be closed and reloaded to take the
-    # environment variable into account.
     connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
     # Create the BlobServiceClient object which will be used to create a container client
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
+    '''
     # Create a unique name for the container
-    container_name = str(uuid.uuid4())
+    #container_name = str(uuid.uuid4())
+    container_name = 'example'
 
     # Create the container
-    container_client = blob_service_client.create_container(container_name)
+    #container_client = blob_service_client.create_container(container_name)
     # Create a local directory to hold blob data
     local_path = "./data"
-    os.mkdir(local_path)
+    #os.mkdir(local_path)
 
     # Create a file in the local data directory to upload and download
-    local_file_name = str(uuid.uuid4()) + ".txt"
+    #local_file_name = str(uuid.uuid4()) + ".txt"
+    local_file_name = "0413.png"
     upload_file_path = os.path.join(local_path, local_file_name)
 
     # Write text to the file
-    file = open(upload_file_path, 'w')
-    file.write("Hello, World!")
-    file.close()
+    #file = open(upload_file_path, 'w')
+    #file.write("Hello, World!")
+    #file.close()
 
     # Create a blob client using the local file name as the name for the blob
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
@@ -41,12 +35,14 @@ try:
     # Upload the created file
     with open(upload_file_path, "rb") as data:
         blob_client.upload_blob(data)
+    '''
     print("\nListing blobs...")
-
+    container_client=blob_service_client.get_container_client('example')
     # List the blobs in the container
     blob_list = container_client.list_blobs()
     for blob in blob_list:
         print("\t" + blob.name)
+
 except Exception as ex:
     print('Exception:')
     print(ex)
---
+++ fetch.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    title: "Test",
+    body: "I am testing!",
+    userId: 1,
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
